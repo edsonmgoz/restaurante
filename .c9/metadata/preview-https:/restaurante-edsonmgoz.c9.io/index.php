fe8051a5cb23a6289216eadffc8874cc9e37bca0
Edson Mollericona Marin <edsonmgoz@gmail.com>
2015-01-29 21:23:26 +0000

    CakePHP instalado y configurado

---
+++ .c9/metadata/preview-https:/restaurante-edsonmgoz.c9.io/index.php
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://restaurante-edsonmgoz.c9.io/index.php","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://restaurante-edsonmgoz.c9.io/index.php","previewer":"preview.browser"},"timestamp":1422566458082,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
